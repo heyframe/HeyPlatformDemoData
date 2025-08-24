a33d4dd17ae802e7e9e05240e19c676d7bee682d
gxs <lyricsong47@gmail.com>
2025-08-25 02:51:02 +0800

    feat: Customer carts are separated in different sales channels

--- src/Bootstrap/CustomerBoostrap.php
+++ src/Bootstrap/CustomerBoostrap.php
@@ -87,7 +87,7 @@ class CustomerBoostrap extends AbstractBootstrap
             [
                 'id' => '0194265f62517068a03a07f37a1580b7',
                 'name' => $this->translationHelper->adjustTranslations([
-                    'zh-CN' => 'VIP 客户组',
+                    'zh-CN' => 'VIP 会员组',
                     'en-GB' => 'VIP customers group',
                 ]),
                 'registrationActive' => true,
@@ -96,21 +96,21 @@ class CustomerBoostrap extends AbstractBootstrap
             [
                 'id' => '0194265f889071d99507a9cc6b09b92f',
                 'name' => $this->translationHelper->adjustTranslations([
-                    'zh-CN' => '新客户组',
+                    'zh-CN' => '新会员组',
                     'en-GB' => 'New customers group',
                 ]),
             ],
             [
                 'id' => '0194265fbcac71f7bd3f7ff0e50ddc92',
                 'name' => $this->translationHelper->adjustTranslations([
-                    'zh-CN' => '测试客户组',
+                    'zh-CN' => '测试会员组',
                     'en-GB' => 'Testing customers group',
                 ]),
             ],
             [
                 'id' => '0194266044b9706a8467c81a3ca36b60',
                 'name' => $this->translationHelper->adjustTranslations([
-                    'zh-CN' => '积分会员客户组',
+                    'zh-CN' => '积分会员会员组',
                     'en-GB' => 'Loyalty Program Members',
                 ]),
             ],
